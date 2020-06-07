c2a0d53dcf3fb6aaf2cdefb79b7dc7986c0ce235
LwcReber <1079670310@qq.com>
2020-06-07 11:04:07 +0800

    git fix

--- admin/src/layout/components/Navbar.vue
+++ admin/src/layout/components/Navbar.vue
@@ -21,11 +21,8 @@
       <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
         <div class="avatar-wrapper">
           <view-avatar class="user-avatar" text="BookLink">
-            <span slot="text">
-              BK
-            </span>
+            <span slot="text">BK</span>
           </view-avatar>
-          <!-- <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar"> -->
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown">
